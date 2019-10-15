28a347516fe9b5bd0dc1f51300445c93769d064e
Chris Mears <chris.mears@gmail.com>
2019-10-14 22:50:05 -0700

    Update docs ✍️

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'Wandering Leaf Studios GatsbyJS website',
+    description: 'Tools for technically-minded entrepreneurs and startups.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -74,4 +73,4 @@ module.exports = {
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
