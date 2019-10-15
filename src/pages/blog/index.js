45e058b569a7910f5902b84f1788fd96d3d834ae
Chris Mears <chris.mears@gmail.com>
2019-10-14 22:51:07 -0700

    Update branding 💅

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,8 +16,8 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              boxShadow: '0.5rem 0 0 rgb(47, 133, 90), -0.5rem 0 0 rgb(47, 133, 90)',
+              backgroundColor: 'rgb(47, 133, 90)',
               color: 'white',
               padding: '1rem',
             }}
--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -30,8 +30,8 @@ export const ProductPageTemplate = ({
       <h2
         className="has-text-weight-bold is-size-1"
         style={{
-          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-          backgroundColor: '#f40',
+          boxShadow: '0.5rem 0 0 rgb(47, 133, 90), -0.5rem 0 0 rgb(47, 133, 90)',
+          backgroundColor: 'rgb(47, 133, 90)',
           color: 'white',
           padding: '1rem',
         }}
